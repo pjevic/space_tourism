3c25f1c3e601117630f83dc9d5c0830045214376
pjevic <milanpjevic@icloud.com>
2023-02-18 09:32:00 +0100

    Implementing Link from React Router

--- src/components/routes/navigation/navigation.component.jsx
+++ src/components/routes/navigation/navigation.component.jsx
@@ -1,11 +1,27 @@
 import { Fragment } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 
 const Navigation = () => {
   return (
     <Fragment>
-      <div>
-        <h1>Navigation</h1>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <div>Logo</div>
+        </Link>
+        <div className="links-container ">
+          <Link className="link" to="/">
+            Home
+          </Link>
+          <Link className="link" to="/destination">
+            Destination
+          </Link>
+          <Link className="link" to="/crew">
+            Crew
+          </Link>
+          <Link className="link" to="/technology">
+            Technology
+          </Link>
+        </div>
       </div>
       <Outlet />
     </Fragment>
